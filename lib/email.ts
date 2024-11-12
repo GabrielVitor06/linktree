63c39f41c2ed31b9186902d961a47788a4a7d61c
BielXL <gabrielvitornordt@gmail.com>
2024-11-12 15:56:01 -0300

    commit

--- lib/email.ts
+++ lib/email.ts
@@ -1,9 +1,12 @@
 import { convert } from "html-to-text";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export function sendEmail(to: string, subject: string, html: string) {
+  if (process.env.NODE_ENV === "development") {
+    return;
+  }
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
   return resend.emails.send({
     from: "novaibiel1234@gmail.com",
     to,
@@ -12,5 +15,3 @@ export function sendEmail(to: string, subject: string, html: string) {
     html,
   });
 }
-
-console.log("Email sent response:", resend.emails);

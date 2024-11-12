f4511dfce06c3cdabf5966f114c1ee2bdc5fc03e
BielXL <gabrielvitornordt@gmail.com>
2024-11-12 15:42:14 -0300

    commit

--- lib/email.ts
+++ lib/email.ts
@@ -1,7 +1,7 @@
 import { convert } from "html-to-text";
 import { Resend } from "resend";
 
-const resend = new Resend(`RESEND_API_KEY`);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export function sendEmail(to: string, subject: string, html: string) {
   if (process.env.NODE_ENV === "development") {

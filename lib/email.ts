ac4e840f133072c1a5d8813f9ca20292f77fa4ba
BielXL <gabrielvitornordt@gmail.com>
2024-11-12 15:53:25 -0300

    commit

--- lib/email.ts
+++ lib/email.ts
@@ -4,10 +4,6 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export function sendEmail(to: string, subject: string, html: string) {
-  if (process.env.NODE_ENV === "development") {
-    return;
-  }
-
   return resend.emails.send({
     from: "novaibiel1234@gmail.com",
     to,

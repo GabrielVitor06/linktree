c80520aa03bc8b90bd1be869ea3e067f8256b55b
BielXL <gabrielvitornordt@gmail.com>
2024-11-12 22:15:48 -0300

    commit

--- lib/email.ts
+++ lib/email.ts
@@ -9,7 +9,7 @@ export function sendEmail(to: string, subject: string, html: string) {
   }
 
   return resend.emails.send({
-    from: "linktree <no-reply@gabrielvitor.dev.com>",
+    from: "<no-reply@gabrielvitor.dev.com>",
     to,
     subject,
     text: convert(html),

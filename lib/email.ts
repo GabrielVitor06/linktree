06775886f46be5131d9a3c362ccaeb8c969b4127
BielXL <gabrielvitornordt@gmail.com>
2024-11-12 21:59:41 -0300

    commit

--- lib/email.ts
+++ lib/email.ts
@@ -9,7 +9,7 @@ export function sendEmail(to: string, subject: string, html: string) {
   }
 
   return resend.emails.send({
-    from: "Acme <onboarding@resend.dev>",
+    from: "no-reply@seudominio.com",
     to,
     subject,
     text: convert(html),

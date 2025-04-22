ae8312a58c4ba345ea90cd8aa20e80d94e964de1
BielXL <gabrielvitornordt@gmail.com>
2025-04-22 14:51:00 -0300

    refactor: move authentication logic to authGoogle module and clean up route handler

--- lib/authGoogle.ts
+++ lib/authGoogle.ts
@@ -1,18 +1,20 @@
+import { AuthOptions, SessionStrategy } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { NextAuthOptions } from "next-auth";
+import db from "@/lib/db";
+import { users } from "@/lib/schema";
+import { createJWT } from "@/lib/JWT";
+import { cookies } from "next/headers";
 
-declare module "next-auth" {
-  interface Session {
-    user: {
-      id?: string;
-      name?: string | null;
-      email?: string | null;
-      image?: string | null;
-    };
-  }
+interface IUser {
+  id: number;
+  name: string | null;
+  email: string;
+  password: string;
+  salt: string;
+  createdAt: string;
 }
 
-export const authOptions: NextAuthOptions = {
+export const authOptions: AuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -20,11 +22,55 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    async session({ session, token }) {
-      if (session.user) {
-        session.user.id = token.sub;
+    async signIn({ user }: { user: unknown }) {
+      const { email, name } = user as IUser;
+
+      if (!email) return false;
+
+      let existingUser = await db.query.users.findFirst({
+        where: (users, { eq }) => eq(users.email, email),
+        columns: { id: true, name: true, email: true },
+      });
+
+      if (!existingUser) {
+        [existingUser] = (await db
+          .insert(users)
+          .values({
+            email,
+            name: name ?? "",
+            password: "",
+            salt: "",
+            createdAt: new Date().toISOString(),
+          })
+          .returning()) as unknown as [IUser];
       }
-      return session;
+
+      const sessionLifetimeInS = 60 * 60 * 24;
+
+      const jwt = await createJWT(
+        {
+          id: existingUser.id,
+          name: existingUser.name,
+          email: existingUser.email,
+        },
+        sessionLifetimeInS
+      );
+
+      (await cookies()).set({
+        name: "session",
+        value: jwt,
+        maxAge: sessionLifetimeInS,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+      });
+
+      return true;
     },
+  },
+  pages: {
+    signIn: "/login",
+  },
+  session: {
+    strategy: "jwt" as SessionStrategy,
   },
 };
